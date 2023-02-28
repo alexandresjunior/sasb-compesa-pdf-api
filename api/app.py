172371fac697ad25ec10a02d46f34bd70aa51490
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-02-28 14:17:23 -0300

    Adição de endpoint de teste

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, make_response
+from reportlab.pdfgen import canvas
+from io import BytesIO
 
 # Cria uma instância do Flask
 app = Flask(__name__)
@@ -6,6 +8,24 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def init():
     return "SASB-COMPESA: Gerador de Relatório de Inspeção em PDF"
+
+@app.route('/relatorio', methods=['GET'])
+def obter_relatorio():
+    # Crie o arquivo PDF usando o ReportLab
+    buffer = BytesIO()
+    pdf = canvas.Canvas(buffer)
+    pdf.drawString(100, 750, "Relatório de Inspeção")
+    pdf.save()
+    
+    # Crie a resposta do Flask com o arquivo PDF em memória
+    buffer.seek(0)
+    
+    response = make_response(buffer.read())
+    
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=relatorio.pdf'
+    
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
