2b497de5cf52e8177b3135000ff96d0213d10010
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-03-01 12:57:52 -0300

    Criação de endpoint temporário de cadastro

--- api/app.py
+++ api/app.py
@@ -5,9 +5,11 @@ from io import BytesIO
 # Cria uma instância do Flask
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def init():
     return "SASB-COMPESA: Gerador de Relatório de Inspeção em PDF"
+
 
 @app.route('/sasb/relatorio', methods=['GET'])
 def obter_relatorio():
@@ -16,17 +18,18 @@ def obter_relatorio():
     pdf = canvas.Canvas(buffer)
     pdf.drawString(100, 750, "Relatório de Inspeção")
     pdf.save()
-    
+
     # Crie a resposta do Flask com o arquivo PDF em memória
     buffer.seek(0)
 
     # Converte o objeto PDF em HTML
     response = make_response(buffer.read())
-    
+
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'attachment; filename=relatorio.pdf'
-    
+
     return response
+
 
 @app.route('/sasb/login', methods=['POST'])
 def login():
@@ -34,19 +37,40 @@ def login():
     data = request.json
     email = data['email']
     senha = data['senha']
-    
+
     # Realize a autenticação e verifique se a senha é correta
     if email == 'teste@compesa.com.br' and senha == 'teste':
         status = 200
         mensagem = 'Autenticação realizada com sucesso!'
     else:
         status = 400
-        mensagem = 'E-mail ou senha inválidos'
+        mensagem = 'E-mail ou senha inválidos! Tente novamente.'
 
     # Retorna um JSON com o status e mensagem da autenticação
     return jsonify({'status': status, 'mensagem': mensagem})
 
+
+@app.route('/sasb/cadastro', methods=['POST'])
+def cadastro():
+    # Obtém os dados do corpo JSON da solicitação
+    data = request.json
+    matricula = data['matricula']
+    email = data['email']
+    senha = data['senha']
+
+    # Realize a autenticação e verifique se a senha é correta
+    if email == 'teste@compesa.com.br' and senha == 'teste':
+        status = 200
+        mensagem = 'Cadastro realizado com sucesso!'
+    else:
+        status = 400
+        mensagem = 'Matrícula inválida! Tente novamente.'
+
+    # Retorna um JSON com o status e mensagem da autenticação
+    return jsonify({'status': status, 'mensagem': mensagem})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-## Run 'python app.py' to initialize this API
+# Run 'python app.py' to initialize this API
